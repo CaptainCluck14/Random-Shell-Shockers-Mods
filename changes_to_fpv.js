a75b5f8c9bc90e1ae7a45d00068b2928fcded9b1
CaptainCluck14 <113867219+CaptainCluck14@users.noreply.github.com>
2023-10-12 13:23:00 -0400

    Update changes_to_fpv.js

--- changes_to_fpv.js
+++ changes_to_fpv.js
@@ -50,28 +50,30 @@
                     /pausing game via pointerlock exit"\)/,
                     /this.id==(\w)&&(\w\w).hide\(\),this.actor.wieldingMelee/,
                     /(\w\w)\[\w\].score\)/,
-                    /this.actor\?this.id==(\w)/
+                    /this.actor\?this.id==(\w)/,
+                    /(\w.weapon).actor.fire/
                 ].map(x => res.match(x));
 
                 if (m.includes(null)) {
                     return;
                 }
-                const [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14] = m;
+                const [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15] = m;
 
                 return res.replace(m1[0], m1[0] + `;${m1[1]}&&(${m1[1]}.actor.showSprites(),${m1[1]}.actor?.hands?.setRenderingGroupId(0),${m1[1]}.meleeWeapon?.actor.weaponMesh?.setRenderingGroupId(0),${m1[1]}.weapon?.actor?.gunMesh?.setRenderingGroupId(0),${m1[1]}.actor?.muzzleFlash?.setRenderingGroupId(0));`)
                     .replace(m2[0], m2[0] + `,${l[1]}.show(),${l[1]}.showDot(),${m2[1]}.actor?.hands?.setRenderingGroupId(2),${m2[1]}.meleeWeapon?.actor?.weaponMesh?.setRenderingGroupId(2),${m2[1]}.weapon?.actor?.gunMesh?.setRenderingGroupId(2),${m2[1]}.actor?.muzzleFlash?.setRenderingGroupId(2),${m2[1]}.actor.hideSprites()`)
                     .replace(m3[0], `restorePreviousPlayerActor(),${l[1]}.hide(),${l[1]}.hideDot()}`)
-                    .replace(m4[0], `const ${c[1]}=${k[1]}[(${a}===${b}.firstPerson)?${d}.playerIdx:${u[1]}];if(!${c[1]}?.weapon)return;${m4[0]}`)// could add scope support
+                    .replace(m4[0], `const ${c[1]}=${k[1]}[(${a}===${b}.firstPerson)?${d}.playerIdx:${u[1]}];if(!${c[1]}?.weapon)return;${m4[0]}`)// could add scope
                     .replace(m5[0], `const ${c[1]}=${k[1]}[(${a}===${b}.firstPerson)?${d}.playerIdx:${u[1]}];if(!${c[1]}?.weapon)return;${m5[0]}`)
                     .replace(m6[0], m6[0] + `||${a}===${b}.firstPerson&&${m6[1]}===${d}.playerIdx`)
                     .replace(m7[0], `wieldGun(),(this.id===${m7[1]}||(${a}===${b}.firstPerson&&${m6[1]}===${d}.playerIdx))`)
                     .replace(m8[0], m8[0] + `||(${a}===${b}.firstPerson&&this.id===${d}.playerIdx)`)
                     .replace(m9[0], m9[0] + `${a}===${b}.firstPerson&&this.id===${d}.playerIdx&&(${l[1]}.hide(),${l[1]}.hideDot()),`)
                     .replace(m10[0], `this.weapon.equip(),(this.id==${m10[1]}||${a}===${b}.firstPerson&&this.id===${d}.playerIdx)`)
-                    .replace(m11[0], m11[0] + `,${d}.restorePreviousPlayerActor(),${d}.unlock()`)
+                    .replace(m11[0], m11[0] + `,${d}.restorePreviousPlayerActor(),${d}.unlock(),${a}=${b}.freeCamera`)
                     .replace(m12[0], `(this.id==${m12[1]}||(this.id==${m10[1]}||${a}===${b}.firstPerson&&${m12[1]}===${d}.playerIdx))&&${m12[2]}.hide(),this.actor.wieldingMelee`)
                     .replace(m13[0], m13[0] + `.sort((a,b)=>${m13[1]}[b].playing-${m13[1]}[a].playing)`)
-                    .replace(m14[0], `this.actor?(this.id==${m14[1]}||${a}===${b}.firstPerson&&this.id===${d}.playerIdx)`);
+                    .replace(m14[0], `this.actor?(this.id==${m14[1]}||${a}===${b}.firstPerson&&this.id===${d}.playerIdx)`)
+                    .replace(m15[0], `${m15[1]}.shootingAccuracy=Math.min(${m15[1]}.shootingAccuracy+${m15[1]}.accuracyLoss,${m15[1]}.accuracyMin),${m15[1]}.accuracyRecover=-(8*${m15[1]}.constructor.accuracyRecover),` + m15[0]);
             }
             return super.response;
 
